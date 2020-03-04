ca179f4b766095e94bc80c3ba4c0a0b60db64aad
jangdonggook <oribal81@daum.net>
2020-03-04 10:31:43 +0900

    .
    .

--- webk/include/js/common.js
+++ webk/include/js/common.js
@@ -1,73 +1,11 @@
 $(function(){
     
-    /*2019.11.25*/   
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 20) {
-            $(".qbtn_top").fadeIn();
-            $(".qbtn_top").addClass("on")
-        } else {
-            $(".qbtn_top").fadeOut();
-            $(".qbtn_top").addClass("on")
-        }
-    });    
-    /*//2019.11.25*/    
-    
-
-	//교재 소개 탭
-	var tabW = 100;
-	var $tabStyle = $(".tab_style2");
-	var $tabList = $tabStyle.find("ul li");
-	var tabLen = $tabList.length;
-	var tabSize = tabW / tabLen;
-	var tabListLastW;
-
-  
-    
-    /* 2019.11.15 수정2 */
-    var tab_li_width = [0,0,0,225.5,180.5,150.8,129.4,113.2]
-	if(tabLen > 3){
-        //$tabList.css({"width":tabSize + "%"});
-        //tabListLastW = $tabStyle.find("ul li").outerWidth();
-        $tabList.css({"width":tab_li_width[tabLen-1] + "px"});
-        $tabList.eq(tabLen-1).css({"width":tabListLastW + tabLen-1}); 
-    }
-    /* //2019.11.15 수정2 */
-    
-    
-	$(document).on("click", ".tab_style2 ul li", jsTabStyleClickEvent);
-
-	function jsTabStyleClickEvent() {
-		var idx = $(this).index();
-		var $list = $(".tab_style2_list");
-
-		$tabList.removeClass("on");
-		$(this).addClass("on");
-		$list.removeClass("active");
-		$list.eq(idx).addClass("active");
-	}
-
-
-	// 브라우저 체크 
+   // 브라우저 체크 
 	var chromeCheckIe = navigator.userAgent.toLocaleLowerCase().indexOf("msie");
 	if(chromeCheckIe > -1) {
 		$("body").addClass("ie");
 	}
-
-	/* quick 관련 */
-	$(".btn_top").click(function(){
-		$( 'html, body' ).animate( { scrollTop : 0 }, 300 );
-		return false;	
-	})
-    /* 2019.11.25 */
-    $(".quick_state_btn").click(function(){
-        
-        $(this).parent().toggleClass("view_on")
-    })
-    /* 2019.11.25 */
-    
-    
-	
-	
+		
 	/* GNB 관련 */
 	$(".gnb > li").bind("mouseenter focusin", function() {
 		//console.log($(this).index() + " 번째 메뉴 over");
@@ -84,51 +22,23 @@ $(function(){
     $(".gnb_all_close").bind("click" , function(){
         $(".gnb_all").removeClass('on');
         
-    })
+	})
+	
+	// 햄버거 메뉴 클릭
 
- 	$(window).scroll(function () {
-    	//console.log('asdasdd')
-        if ($(this).scrollTop() > 50) {
-            $(".quick_menu_wrap").addClass("on")         
-        } else {
-            $(".quick_menu_wrap").removeClass("on");         
-        }
-        
+	$(".ham").click(function(){
+		$(this).find(".menu-trigger").toggleClass("active-1")
+	})
 
-        if ($(this).scrollTop() > 250  && $(this).scrollTop() < 1500) {            
-            $(".gnb_all_close").addClass("on")
-        } else {           
-            $(".gnb_all_close").removeClass("on");
-        }
-    });
+	
 
 
-	/*고객센터 자주하는질문 */
-	$(".faq_list li .q_title").click(function(){
-		$(this).parent(".faq_list li").toggleClass("active");
-		$(this).next(".faq_list li .a_con").slideToggle();
-		return false;
-	});
-
-	//교재자료실 동영상 스크랩 버튼 클릭시
-	$(".video_list .good_mark").click(function(){
-		$(this).toggleClass('on');
-	});
-
-	// notice
-	$(document).on("click", ".contact_list li .list", jsSlideToggleEvent);
-
-	function jsSlideToggleEvent() {
-		$(this).next(".contact_list li .con").slideToggle();
-	}
-
-	//MY AVA 내 교재 관리
-	$("div.case > a").click(function(e){
-		$(this).toggleClass('on');
-		return false;
-	});
 
 
+
+
+
+	
 
 
 
